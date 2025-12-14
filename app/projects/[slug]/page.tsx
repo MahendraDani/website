f37a69c6cf92364baeb282950e90c2b1f1ecfe99
Mahendra Dani <danimahendra0904@gmail.com>
2025-12-14 16:01:30 +0530

    fix(ui): update projects page header
    Signed-off-by: Mahendra Dani <danimahendra0904@gmail.com>

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,12 +1,17 @@
 import { projects } from "#site/content";
 import { MDXContentRenderer } from "@/components/mdx/mdx-content-renderer";
 import { notFound } from "next/navigation";
-import {formatDateAndTime} from "@/lib/date"
+import { formatDateAndTime } from "@/lib/date";
 import { FadeUp } from "@/components/fade-up";
 import { siteConfig } from "@/configs/site.config";
 import { Metadata } from "next";
 import dayjs from "dayjs";
 import { A } from "@/components/a";
+import Link from "next/link";
+import { LinkedInIcon } from "@/components/icon/linkedin";
+import { GithubIcon } from "@/components/icon/github";
+import { TwitterIcon } from "@/components/icon/twitter";
+import { GlobeIcon, NotebookPenIcon, VideoIcon } from "lucide-react";
 
 interface ProjectPageParams {
   params: {
@@ -14,10 +19,12 @@ interface ProjectPageParams {
   };
 }
 
-export async function generateStaticParams() : Promise<ProjectPageParams["params"][]>{
-  return  projects.map((project)=>({
-    slug : project.slugAsParams
-  }))
+export async function generateStaticParams(): Promise<
+  ProjectPageParams["params"][]
+> {
+  return projects.map((project) => ({
+    slug: project.slugAsParams,
+  }));
 }
 
 function getProjectFromParams(params: { slug: string }) {
@@ -62,7 +69,7 @@ export default async function ProjectPage({ params }: ProjectPageParams) {
     <FadeUp>
       <div>
         <article className="relative">
-          <div>
+          {/* <div>
             <p className="scroll-m-20 text-xl font-bold tracking-tight lg:text-2xl [&:not(:first-child)]:mt-6">
               {project.name}
             </p>
@@ -94,6 +101,57 @@ export default async function ProjectPage({ params }: ProjectPageParams) {
               ))}
             </div>
             }
+          </div> */}
+          <div className="space-y-4 border py-4 px-4 mb-2 bg-secondary border-dashed">
+            <h1 className="uppercase text-xl md:text-3xl font-bold text-center">
+              {project.name}
+            </h1>
+            <div className="space-y-2">
+              <p className="italic text-sm text-center text-muted-foreground">
+                {formatDateAndTime(project.publishDate)}
+              </p>
+              <p className="italic text-sm text-pretty text-center">
+                {project.excerpt}
+              </p>
+              <div className="text-center text-wrap space-x-2">
+                {project.stack.map((stack, idx) => (
+                  <span
+                    key={idx}
+                    className="italic captialize text-sm text-center text-muted-foreground"
+                  >
+                    {stack}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-center gap-2">
+                {project.links.map((link, idx) => (
+                  <Link key={idx} href={link.url} target="_blank">
+                    {link.type == "linkedin" && (
+                      <LinkedInIcon className="h-6 w-6 p-1 border hover:bg-accent/15 hover:border-accent duration-300" />
+                    )}
+                    {link.type == "github" && (
+                      <GithubIcon className="h-6 w-6 p-1 border hover:bg-accent/15 hover:border-accent duration-300" />
+                    )}
+                    {link.type == "twitter" && (
+                      <TwitterIcon className="h-6 w-6 p-1 border hover:bg-accent/15 hover:border-accent duration-300" />
+                    )}
+                    {link.type == "live" && (
+                      <GlobeIcon className="h-6 w-6 p-1 border hover:bg-accent/15 hover:border-accent duration-300" />
+                    )}
+                    {link.type == "blog" && (
+                      <NotebookPenIcon className="h-6 w-6 p-1 border hover:bg-accent/15 hover:border-accent duration-300" />
+                    )}
+                    {(link.type == "demoVideo" || link.type == "youtube") && (
+                      <VideoIcon className="h-6 w-6 p-1 border hover:bg-accent/15 hover:border-accent duration-300" />
+                    )}
+                    {link.type == "other" && (
+                      <GlobeIcon className="h-6 w-6 p-1 border hover:bg-accent/15 hover:border-accent duration-300" />
+                    )}
+                  </Link>
+                ))}
+              </div>
+              
+            </div>
           </div>
           <div className="mx-auto sm:w-auto min-w-0 text-justify">
             <MDXContentRenderer code={project.body} />
