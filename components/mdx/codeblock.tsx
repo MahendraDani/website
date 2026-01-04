8d35c4e1c4e32a0058e48065a93960007dc8b747
Mahendra Dani <danimahendra0904@gmail.com>
2026-01-04 13:13:48 +0530

    project: add gitloom
    Signed-off-by: Mahendra Dani <danimahendra0904@gmail.com>

--- components/mdx/codeblock.tsx
+++ components/mdx/codeblock.tsx
@@ -7,7 +7,7 @@ export const CodeBlock = ({
   ...props
 }: React.HTMLAttributes<HTMLPreElement>) => {
   return (
-    <div className="relative font-mono text-sm overflow-x-auto px-1 py-2 bg-[#22272E]">
+    <div className="relative font-mono text-sm overflow-x-auto mt-2 px-1 py-2 bg-[#22272E]">
       {/* <div className="flex justify-end py-1 pr-3 bg-black/70 text-gray-300 rounded-t-lg border-t border-l border-r">
         <CopyButton>{children}</CopyButton>
       </div> */}
